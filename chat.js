5e681dd4fde8aea5455aa1508240b82abd2602c1
rutvik <rutvik.mohite@bytestechnolab.com>
2025-05-14 19:37:21 +0530

    cddcdc

--- chat.js
+++ chat.js
@@ -1,47 +1,144 @@
-var socket = io.connect("http://127.0.0.1:3000");
+const SECRET_KEY = "mychatappkey458755!";
+const socket = io("http://localhost:3000");
 
-socket.on('connect', () => {
-    // console.log(socket.id);
-    $('#socket_id').val(socket.id)
+let userName = '';
+let userAvatar = '';
+let partnerName = '';
+let partnerAvatar = '';
+let currentRoom = null;
+let isDarkMode = false;
+
+socket.on('connect', function () {
+    mySocketId = socket.id;
 });
 
-socket.once("message", (mess) => {
-    // console.log(mess)
+// Dark Mode Toggle
+$('#darkModeToggle').click(function () {
+    $('body').toggleClass('dark-mode');
+    isDarkMode = !isDarkMode;
 });
 
-socket.on('creatroom', (room) => {
-    // console.log(room);
-    $('#room_id').val(room)
-});
+// Modal logic
+const modal = new bootstrap.Modal(document.getElementById('userModal'));
+modal.show();
 
-$("#send").click(function() {
-    let roomname = $('#room_id').val()
-    let message = $('#text').val()
-    let socket_id = $('#socket_id').val()
-    if(message!=''){
-    socket.emit("sendmes", roomname, message, socket_id);
-    }
-    $('#text').val('')
-});
-
-$('#text').keypress(function(e) {
-    var key = e.which;
-    let roomname = $('#room_id').val()
-    let message = $('#text').val()
-    let socket_id = $('#socket_id').val()
-    if (key == 13 && message!='') // the enter key code
-    {
-      socket.emit("sendmes", roomname, message, socket_id);
-      $('#text').val('')
+$('.avatar-select').click(function () {
+    $('.avatar-select').removeClass('border border-primary');
+    $(this).addClass('border border-primary');
+    $('#avatarChoice').val($(this).data('avatar'));
+    if ($('#termsCheck').is(':checked')) {
+        $('#joinBtn').prop('disabled', !$('#username').val());
     }
 });
 
-socket.on('sendmessage', (mes, id) => {
-    let socket_id = $('#socket_id').val()
-    $('#text').val('')
-    if (socket_id == id) {
-        $('.msg-text').last().after('<div class="msg-text owner"><span class="text">' + mes + '</span></div>')
+$('#username').on('input', function () {
+    if ($('#termsCheck').is(':checked')) {
+        $('#joinBtn').prop('disabled', !$(this).val() || !$('#avatarChoice').val());
+    }
+});
+
+$('#termsCheck').on('change', function () {
+    if ($(this).is(':checked')) {
+        $('#joinBtn').prop('disabled', !$('#username').val() || !$('#avatarChoice').val());
     } else {
-        $('.msg-text').last().after('<div class="msg-text"><span class="text">' + mes + '</span></div>')
+        $('#joinBtn').prop('disabled', true);
+    }
+});
+
+$('#joinBtn').click(function () {
+    userName = $('#username').val().trim();
+    userAvatar = $('#avatarChoice').val() === 'male'
+        ? 'man.png'
+        : 'human.png';
+
+    socket.emit('user_info', { name: userName, avatar: userAvatar });
+    modal.hide();
+});
+
+socket.on('waiting', msg => $('#status').text(msg));
+
+socket.on('room_created', data => {
+    currentRoom = data.room;
+    partnerName = data.partner.name;
+    partnerAvatar = data.partner.avatar;
+
+    $('#status').text("You're connected!");
+    $('#partnerName').text(partnerName);
+    $('#partnerAvatar').attr('src', partnerAvatar);
+    $('#partnerInfo').removeClass('d-none');
+
+    $('#chat').removeClass('d-none');
+    $('#messageControls').removeClass('d-none');
+});
+
+$('#send').click(function () {
+    $('#emojiPicker').hide();
+    const message = $('#input').val().trim();
+    if (!message || !currentRoom) return;
+
+    const encrypted = CryptoJS.AES.encrypt(message, SECRET_KEY).toString();
+
+    socket.emit('encrypted_message', {
+        room: currentRoom,
+        encrypted
+    });
+
+    $('#chat').append(`
+    <div class="message you">
+        ${message}
+    </div>
+`);
+
+    $('#input').val('');
+    $('#chat').scrollTop($('#chat')[0].scrollHeight);
+});
+
+$('#input').on('keypress', function (e) {
+    if (e.which === 13 && !e.shiftKey) {
+        e.preventDefault();
+        $('#send').click();
+    }
+})
+
+socket.on('receive_encrypted', function (data) {
+    const decrypted = CryptoJS.AES.decrypt(data.encrypted, SECRET_KEY).toString(CryptoJS.enc.Utf8);
+    $('#chat').append(`
+    <div class="message partner">
+        ${decrypted}
+    </div>
+`);
+    $('#chat').scrollTop($('#chat')[0].scrollHeight);
+});
+
+// Typing indicator
+$('#input').on('input', function () {
+    socket.emit('typing', currentRoom);
+});
+
+socket.on('partner_typing', function () {
+    $('#typingIndicator').text(partnerName + ' is typing...')
+    $('#typingIndicator').show();
+    setTimeout(() => $('#typingIndicator').hide(), 3000);
+});
+
+socket.on('partner_disconnected', () => {
+    $('#chat').append('<div class="text-muted text-center">' + partnerName + '   has disconnected.</div>');
+    $('#messageControls').hide();
+});
+
+// Emoji Picker
+$('#emojiBtn').click(function () {
+    $('#emojiPicker').toggle();
+});
+
+$('#emojiPicker button').click(function () {
+    const emoji = $(this).data('emoji');
+    $('#input').val($('#input').val() + emoji);
+    // $('#emojiPicker').hide();
+});
+
+socket.on('seen_by_partner', function (senderId) {
+    if (senderId === mySocketId) {
+        $('.status').last().text('✓✓ Seen');
     }
 });
